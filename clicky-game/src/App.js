cc57862280fc0c03ebdfca4e5755ad1f016e6586
Jhayla Young <jhayla.young@gmail.com>
2020-02-23 12:37:27 -0600

    working on styles, got the functions to kinda work

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CharaCard from "./CharaCard";
+import Wrapper from "./Wrapper";
+import Navbar from "./Wrapper";
+import Jumbotron from "./Jumbotron";
+import characters from "./characters.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    characters,
+    highScore: 0,
+    currentScore: 0,
+    Clicked: false
+  };
+
+  handleClick = id => {
+    this.shufflyArray();
+    this.scoreHandle(id);
+    console.log(this.state.timesClicked);
+  };
+
+  scoreHandle = id => {
+    this.state.characters.forEach(element => {
+      if (id === element.id && element.clicked == false) {
+        element.clicked = true;
+        this.setState({ Clicked: false });
+        this.incrementHandle();
+      } else if (id === element.id && element.clicked === true) {
+        if (this.state.currentScore > this.state.highScore) {
+          this.setState({ highScore: this.state.currentScore });
+        }
+        this.setState({ currentScore: 0 });
+        this.setState({ Clicked: true });
+        this.state.characters.forEach(element => (element.clicked = false));
+        console.log(this.state.characters);
+      }
+    });
+  };
+
+  shufflyArray = () => {
+    ///array for the shuffle///
+
+    const shuffleArry = this.shuffle(this.state.characters);
+
+    this.setState({ shuffleArry });
+  };
+
+  incrementHandle = () => {
+    this.setState({ currentScore: this.state.currentScore + 1 });
+
+  };
+
+  ///taking array parameters and shuffle///
+
+  shuffle = array => {
+    var currentIndx = array.lenght,
+      temporaryVal,
+      randomIndx;
+
+    while (0 !== currentIndx) {
+      randomIndx = Math.floor(Math.random() * currentIndx);
+      currentIndx -= 1;
+
+      //swap the element//
+      temporaryVal = array[currentIndx];
+      array[currentIndx] = array[randomIndx];
+      array[randomIndx] = temporaryVal;
+    }
+
+    return array;
+
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Navbar
+          currentScore={this.state.currentScore}
+          highScore={this.state.highScore} />
+
+        <Jumbotron />
+        {this.state.characters.map(character => (
+          <CharaCard
+            Clicked={this.state.Clicked}
+            handleClick={this.handleClick}
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image} />
+        ))}
+      </Wrapper>
+    );
+  }
+
 }
+
 
 export default App;
